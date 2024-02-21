8ee2c63d062ee70d059662c56b91b7794a96df30
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-21 16:06:42 +0100

    login and register

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homePage');
 });
+
+Route::get('/login', function(){
+    return view('Authentication.authentication');
+});
+
+Route::get('/register', function(){
+    return view('Authentication.authentication');
+});
