4e25a7e70fbc4f5851a283bd6dae8322b955a826
kholoud001 <kholoud.sanak@gmail.com>
2024-02-22 23:33:17 +0100

    LoginController
    Logout function too

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,65 @@
+<?php
+
+
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\Client;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function login (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+//            dd(Hash::check($request->password, $user->password));
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token];
+                //return response($response, 200);
+                return redirect('/')->with('success', 'Login successful!');
+
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" =>'User does not exist'];
+           // return response($response, 422);
+            return redirect('/login')->with('error', 'Invalid credentials. Please try again.');
+
+        }
+    }
+
+
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $accessToken = $request->user()->token();
+        $accessToken->revoke();
+
+        return redirect('/login')->with('success', 'Logout successful!');
+    }
+
+}
