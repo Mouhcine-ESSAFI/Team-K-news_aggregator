082c02d8d6125ba733a542e6599cc735a9587b2e
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-22 16:18:19 +0100

    add form to add rss compnenet

---
+++ resources/views/components/forms/rss-form.blade.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="flex flex-wrap">
+        <div class="w-full max-w-full px-3 mx-auto mt-0 md:flex-0 shrink-0">
+            <div
+                class="relative z-0 flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
+                <div class="p-6 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
+                    <h5>Add Rss Form</h5>
+                </div>
+                <div class="flex-auto p-6">
+                    <form role="form text-left">
+                        <div class="mb-4">
+                            <input type="text"
+                                   class="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+                                   placeholder="Name" aria-label="Name" aria-describedby="email-addon"/>
+                        </div>
+                        <div class="mb-4 flex justify-between">
+                            <input type="email"
+                                   class="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+                                   placeholder="Email" aria-label="Email" aria-describedby="email-addon"/>
+                            <form class="max-w-sm ml-4 mx-auto">
+                                <select id="countries" class="bg-gray-50 ml-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected>Choose a category</option>
+                                    <option value="US">United States</option>
+                                    <option value="CA">Canada</option>
+                                    <option value="FR">France</option>
+                                    <option value="DE">Germany</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div class="text-center">
+                            <button type="button"
+                                    class="inline-block w-full px-6 py-3 mt-6 mb-2 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-gray-900 to-slate-800 hover:border-slate-700 hover:bg-slate-700 hover:text-white">
+                                ADD A RSS
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
