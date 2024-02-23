ce8a58780f15ae0bfb0c49b16a59b3e63fdb279a
Mouhcine Essafi <messafi1337@gmail.com>
2024-02-23 11:07:57 +0100

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Categories::all();
-        return view('dashboard', compact('categories'));
+        return view('admin.category', compact('categories'));
     }
 
     public function store(Request $request)
@@ -23,7 +23,7 @@ class CategoryController extends Controller
             'name' => $request->name
         ]);
 
-        return redirect()->route('dashboard.category');
+        return redirect()->route('home');
     }
 
     public function destroy($id)
@@ -31,9 +31,8 @@ class CategoryController extends Controller
         $category = Categories::findOrFail($id);
         $category->delete();
 
-        return redirect()->route('dashboard.category');
+        return redirect()->route('home');
     }
-
 
     public function displayCategories()
     {
