6f755af7d693e196e0e28250f8ea64168a929cf7
alifertah <alifertah42@gmail.com>
2024-02-23 18:33:42 +0100

    fix : adding the category dynamically

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,20 +9,29 @@ class PostController extends Controller
 {
     public function insertPost(Request $r)
     {
-        $rss_feed_url = 'https://www.france24.com/fr/rss';
+        $catRssLink = [
+        "economy" => "https://www.france24.com/fr/%C3%A9co-tech/rss",
+        "sport" => "https://www.france24.com/fr/sports/rss",
+        "culture" => "https://www.france24.com/fr/culture/rss"];
+
         
-        $rss_feed_data = file_get_contents($rss_feed_url);
-        $rss = simplexml_load_string($rss_feed_data);
-        
-        foreach ($rss->channel->item as $item) {
-            $p = new Post();
-        
-            $p->title = $item->title;
-            $p->description = $item->description;
-            $p->category = $item->category;
-            $p->image = $item->enclosure['url'];
+        foreach($catRssLink as $category => $rss){
+            $rss_feed_data = file_get_contents($rss);
+            $rss = simplexml_load_string($rss_feed_data);
             
-            $p->save();
+            foreach ($rss->channel->item as $item) {
+                $p = new Post();
+            
+                $p->title = $item->title;
+                $p->description = $item->description;
+                $p->category = $category;
+                $p->image = $item->enclosure['url'];
+                
+                $p->save();
+            }
         }
+
+
+
     }
 }
