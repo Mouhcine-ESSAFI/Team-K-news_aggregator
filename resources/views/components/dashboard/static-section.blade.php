6179ed8af1f78c59acf6ea55b78fe892207ffb7f
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-24 16:53:31 +0100

    Update static-section.blade.php

--- resources/views/components/dashboard/static-section.blade.php
+++ resources/views/components/dashboard/static-section.blade.php
@@ -1,4 +1,4 @@
-<main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
+<main class="ease-soft-in-out xl:ml-68.5 relative h-screen max-h-screen rounded-xl transition-all duration-200">
     <!-- cards -->
     <div class="w-full px-6 py-6 mx-auto">
         <!-- row 1 -->
@@ -107,8 +107,27 @@
             <div class="w-full px-3 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
                 <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
                     <div class="flex-auto p-4">
-                        <canvas id="userChart" class="w-full" height="150"></canvas>
-
+                        <div class="flex flex-wrap -mx-3">
+                            <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
+                                <div class="flex flex-col h-full">
+                                    <p class="pt-2 mb-1 font-semibold">Built by developers</p>
+                                    <h5 class="font-bold">Soft UI Dashboard</h5>
+                                    <p class="mb-12">From colors, cards, typography to complex elements, you will find the full documentation.</p>
+                                    <a class="mt-auto mb-0 font-semibold leading-normal text-sm group text-slate-500" href="javascript:;">
+                                        Read More
+                                        <i class="fas fa-arrow-right ease-bounce text-sm group-hover:translate-x-1.25 ml-1 leading-normal transition-all duration-200"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="max-w-full px-3 mt-12 ml-auto text-center lg:mt-0 lg:w-5/12 lg:flex-none">
+                                <div class="h-full bg-gradient-to-tl from-purple-700 to-pink-500 rounded-xl">
+                                    <img src="../assets/img/shapes/waves-white.svg" class="absolute top-0 hidden w-1/2 h-full lg:block" alt="waves" />
+                                    <div class="relative flex items-center justify-center h-full">
+                                        <img class="relative z-20 w-full pt-6" src="../assets/img/illustrations/rocket-white.png" alt="rocket" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -130,30 +149,10 @@
         </div>
 
 
-        <div class="w-full py-3 mb-10 lg:mb-0 lg:flex-none">
+        <div class="w-full h-48 pt-8 mb-10 lg:mb-0 lg:flex-none">
             <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
                 <div class="flex-auto p-4">
-                    <div class="flex flex-wrap -mx-3">
-                        <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
-                            <div class="flex flex-col h-full">
-                                <p class="pt-2 mb-1 font-semibold">Built by developers</p>
-                                <h5 class="font-bold">Soft UI Dashboard</h5>
-                                <p class="mb-12">From colors, cards, typography to complex elements, you will find the full documentation.</p>
-                                <a class="mt-auto mb-0 font-semibold leading-normal text-sm group text-slate-500" href="javascript:;">
-                                    Read More
-                                    <i class="fas fa-arrow-right ease-bounce text-sm group-hover:translate-x-1.25 ml-1 leading-normal transition-all duration-200"></i>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="max-w-full px-3 mt-12 ml-auto text-center lg:mt-0 lg:w-5/12 lg:flex-none">
-                            <div class="h-full bg-gradient-to-tl from-purple-700 to-pink-500 rounded-xl">
-                                <img src="../assets/img/shapes/waves-white.svg" class="absolute top-0 hidden w-1/2 h-full lg:block" alt="waves" />
-                                <div class="relative flex items-center justify-center h-full">
-                                    <img class="relative z-20 w-full pt-6" src="../assets/img/illustrations/rocket-white.png" alt="rocket" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <canvas id="userChart" class="w-full h-full" height="150"></canvas>
                 </div>
             </div>
         </div>
@@ -162,3 +161,49 @@
     <!-- end cards -->
 </main>
 
+<script>
+
+    console.log("salma");
+        
+    const dates = @json($userStatistics->pluck('date'));
+    const counts = @json($userStatistics->pluck('user_count'));
+
+    const ctx = document.getElementById('userChart').getContext('2d');
+    const userChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'Nombre d\'abonnements ajoutés',
+                data: counts,
+                backgroundColor: 'rgba(linear-gradient(to right, #8B5CF6, #FF7A67))',
+                borderColor: 'rgba(233, 83, 148, 1)',                
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Nombre d\'abonnements'
+                    }
+                }
+            }
+        }
+    });
+
+    function pdf()
+    {
+        const element = document.getElementById('statistique');
+        html2pdf(element);
+    }
+          
+</script>
