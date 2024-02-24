69e5ebb64db2a08df8ba9b0c2d8d8b6f6ed3ab89
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-24 16:17:42 +0100

    Update static-section.blade.php

--- resources/views/components/dashboard/static-section.blade.php
+++ resources/views/components/dashboard/static-section.blade.php
@@ -35,10 +35,10 @@
                         <div class="flex flex-row -mx-3">
                             <div class="flex-none w-2/3 max-w-full px-3">
                                 <div>
-                                    <p class="mb-0 font-sans font-semibold leading-normal text-sm">Today's Users</p>
+                                    <p class="mb-0 font-sans font-semibold leading-normal text-sm">Total Users</p>
                                     <h5 class="mb-0 font-bold">
-                                        2,300
-                                        <span class="leading-normal text-sm font-weight-bolder text-lime-500">+3%</span>
+                                        {{ $totalUsers }}
+                                        <span class="leading-normal text-sm font-weight-bolder text-lime-500">Utilisateurs</span>
                                     </h5>
                                 </div>
                             </div>
@@ -107,27 +107,8 @@
             <div class="w-full px-3 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
                 <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
                     <div class="flex-auto p-4">
-                        <div class="flex flex-wrap -mx-3">
-                            <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
-                                <div class="flex flex-col h-full">
-                                    <p class="pt-2 mb-1 font-semibold">Built by developers</p>
-                                    <h5 class="font-bold">Soft UI Dashboard</h5>
-                                    <p class="mb-12">From colors, cards, typography to complex elements, you will find the full documentation.</p>
-                                    <a class="mt-auto mb-0 font-semibold leading-normal text-sm group text-slate-500" href="javascript:;">
-                                        Read More
-                                        <i class="fas fa-arrow-right ease-bounce text-sm group-hover:translate-x-1.25 ml-1 leading-normal transition-all duration-200"></i>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="max-w-full px-3 mt-12 ml-auto text-center lg:mt-0 lg:w-5/12 lg:flex-none">
-                                <div class="h-full bg-gradient-to-tl from-purple-700 to-pink-500 rounded-xl">
-                                    <img src="../assets/img/shapes/waves-white.svg" class="absolute top-0 hidden w-1/2 h-full lg:block" alt="waves" />
-                                    <div class="relative flex items-center justify-center h-full">
-                                        <img class="relative z-20 w-full pt-6" src="../assets/img/illustrations/rocket-white.png" alt="rocket" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <canvas id="userChart" class="w-full" height="150"></canvas>
+
                     </div>
                 </div>
             </div>
@@ -180,3 +161,4 @@
 </div>
     <!-- end cards -->
 </main>
+
