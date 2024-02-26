2f8062cdfb02794567ae2d49d9b88f5796098232
kholoud001 <kholoud.sanak@gmail.com>
2024-02-26 22:39:38 +0100

    Auth Login Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,57 +9,61 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Laravel\Passport\Client;
+use Laravel\Passport\Token;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    public function login (Request $request) {
+    public function login(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string|min:8',
         ]);
-        if ($validator->fails())
-        {
-            return response(['errors'=>$validator->errors()->all()], 422);
+
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
         }
+
         $user = User::where('email', $request->email)->first();
 
         if ($user) {
-
             if (Hash::check($request->password, $user->password)) {
                 $token = $user->createToken('Laravel Password Grant Client')->accessToken;
                 $response = ['token' => $token];
-                //return response($response, 200);
-                return redirect('/preferences')->with('success', 'Login successful!');
+                $request->session()->regenerate();
+                $request->session()->put('user_id', $user->id);
+                //dd($request->session()->get('user_id'));
 
+
+                // Check user role and redirect accordingly
+                if ($user->role === 'admin') {
+                    return redirect('/dashboard')->with('success', 'Login successful!');
+                } else {
+                    return redirect('/preferences')->with('success', 'Login successful!');
+                }
             } else {
                 $response = ["message" => "Password mismatch"];
                 return response($response, 422);
             }
         } else {
-            $response = ["message" =>'User does not exist'];
-           // return response($response, 422);
+            $response = ["message" => 'User does not exist'];
             return redirect('/login')->with('error', 'Invalid credentials. Please try again.');
-
         }
     }
 
 
 
-    /**
-     * Log the user out of the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
     public function logout(Request $request)
     {
-        Auth::logout();
+        $request->user()->token()->revoke();
 
-        $accessToken = $request->user()->token();
-        $accessToken->revoke();
+        // Clear the user's session data
+        $request->session()->forget('user_id');
 
+        // Redirect to the login page with a success message
         return redirect('/login')->with('success', 'Logout successful!');
     }
 
