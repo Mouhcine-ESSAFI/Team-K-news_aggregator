1397ef4ca5195f8b0ade88eb605f3950d66d71a1
alifertah <alifertah42@gmail.com>
2024-02-26 15:02:05 +0100

    feat : changing the category in posts table from string to int and giving it the id of the category selected from rss links table

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,19 +11,20 @@ class PostController extends Controller
 {
     public function insertPost(Request $r)
     {
-        $catRssLink = new SourceRss();
-        $catRssLink->all();
+        $rssToInsert = new SourceRss();
+        $catRssLink = $rssToInsert->all();
         
-        foreach($catRssLink as $category => $rss){
-            $rss_feed_data = file_get_contents($rss);
+
+        foreach($catRssLink as $rss){
+            $category = $rss->category_id;
+            $rss_feed_data = file_get_contents($rss->rss_link);
             $rss = simplexml_load_string($rss_feed_data);
             
             foreach ($rss->channel->item as $item) {
                 $p = new Post();
-            
                 $p->title = $item->title;
                 $p->description = $item->description;
-                $p->category = $category;
+                $p->category_id = $category;
                 $p->image = $item->enclosure['url'];
                 $p->save();
             }
