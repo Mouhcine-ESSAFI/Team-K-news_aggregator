72d85349e845ea2c90385086184ab64dccf7ec57
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-26 09:16:58 +0100

    ajouter une methode pour afficher la formulaire avec les categories

--- app/Http/Controllers/RssManage.php
+++ app/Http/Controllers/RssManage.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categories;
 use App\Models\SourceRss;
 use Illuminate\Http\Request;
 
 class RssManage extends Controller
 {
+
+    /***
+     * show all categories in the form
+     */
+    public function index()
+    {
+        $categories = Categories::all();
+        return view('dashboard', compact('categories'));
+    }
     /**
      * inserts a new rss to source table
      */
     public function newRss(Request $r){
         $source = new SourceRss();
 
-        $source->category_id = 1;
+        $source->category_id = $r->category;
         $source->rss_link = $r->link;
         $source->name = $r->name;
         $source->save();
