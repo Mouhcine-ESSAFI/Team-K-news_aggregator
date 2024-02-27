ef7d670115533d26e9be570ab623e42e029bb5ec
kholoud001 <kholoud.sanak@gmail.com>
2024-02-27 21:49:33 +0100

    preference controller1 update

---
+++ app/Http/Controllers/preferenceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Preference;
+
+class preferenceController extends Controller
+{
+        public function addPreference(Request $request)
+        {
+            $request->validate([
+                'selected_tags' => 'required|array', 
+            ]);
+        
+            $idUser = '1'; 
+        
+            foreach ($request->selected_tags as $categoryId) {
+                Preference::create([
+                    'category_id' => $categoryId,
+                    'user_id' => $idUser,
+                ]);
+            }
+        
+            return redirect()->route('preferences.show');
+        }
+
+
+}
