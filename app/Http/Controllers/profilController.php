dd215586404ab17038d065ac9d56ab2c81079bc7
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-27 10:49:12 +0100

    Create profilController.php

---
+++ app/Http/Controllers/profilController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Preference;
+use App\Models\Categories;
+
+
+class profilController extends Controller
+{
+    public function showProfil()
+    {
+        $idUser = session()->get('user_id');
+
+        $userInfos = [
+            $username = session()->get('user_name'),
+            $email = session()->get('user_email')
+        ];
+
+        $preferences = Preference::where('user_id', $idUser)->get();
+        
+        $categories = [];
+        
+        foreach ($preferences as $preference) {
+            $categoryId = $preference->category_id;
+        
+            $category = Categories::find($categoryId);
+        
+            if ($category) {
+                $categories[] = $category;
+            }
+        }
+        
+        return view('Authentication.authentication', compact('categories','userInfos'));
+    }
+
+
+}
