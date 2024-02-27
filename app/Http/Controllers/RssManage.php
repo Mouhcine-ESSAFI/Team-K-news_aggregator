f94c12ea5c779ab6a32553cb881dfbf6a6a045d2
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-27 15:35:47 +0100

    ajouter une methode pour envoyer l'email

--- app/Http/Controllers/RssManage.php
+++ app/Http/Controllers/RssManage.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\NewNotification;
 use App\Models\Categories;
+use App\Models\Post;
+use App\Models\Preference;
 use App\Models\SourceRss;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class RssManage extends Controller
 {
@@ -12,7 +17,6 @@ class RssManage extends Controller
     /***
      * show all categories in the form
      * show all rss links
-
      */
     public function index()
     {
@@ -26,13 +30,32 @@ class RssManage extends Controller
     /**
      * inserts a new rss to source table
      */
-    public function newRss(Request $r){
+    public function newRss(Request $r)
+    {
         $source = new SourceRss();
         $source->category_id = $r->category_id;
         $source->rss_link = $r->link;
         $source->name = $r->name;
         $source->save();
+        $post = 0;
+        $this->sendEmailNotificationToInterestedUsers($r->category_id);
+
         return redirect('/Rss');
+    }
+
+    /***
+     * envoyer un email apres l'insertion des postes
+     */
+    private function sendEmailNotificationToInterestedUsers($categories)
+    {
+        $preferences = Preference::where('category_id', $categories)->get();
+
+        foreach ($preferences as $preference) {
+            $user = User::find($preference->user_id); // Assuming there's a relationship between Preference and User
+            if ($user) {
+                Mail::to($user->email)->send(new NewNotification($user));
+            }
+        }
     }
 
     /**
