98f12474be99960b83cb4f3e62b837f3f682fb06
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-28 12:31:34 +0100

    Update collectionPage.blade.php

--- resources/views/News/collectionPage.blade.php
+++ resources/views/News/collectionPage.blade.php
@@ -34,15 +34,39 @@
                                 <div class="">
                                     <div class="relative h-62 w-full mb-3">
                                         <div class="absolute flex flex-col top-0 right-0 p-3">
-                                            <button
-                                                class="transition ease-in duration-300 bg-gray-800  hover:text-red-500 shadow hover:shadow-md text-gray-500 rounded-full w-8 h-8 text-center p-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                                     viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          stroke-width="2"
-                                                          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
-                                                </svg>
-                                            </button>
+                                            {{-- dislike --}}
+                                            @if ($favoris->contains($post->id))
+                                                <form action="{{ route('removeToFavoris') }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <input type="hidden" value="{{$post->id}} " name="postId">
+                                                    <button type="submit" 
+                                                        class="transition ease-in duration-300 bg-gray-800  text-red-500 shadow hover:shadow-md hover:text-gray-500 rounded-full w-8 h-8 text-center p-1">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                            viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                stroke-width="2"
+                                                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
+                                                        </svg>
+                                                    </button>
+                                                </form>
+                                            @else
+                                            {{-- like --}}
+                                            <form action="{{ route('addToFavoris') }}" method="POST">
+                                                @csrf
+                                                <input type="hidden" value="{{$post->id}} " name="postId">
+                                                <button type="submit" 
+                                                    class="transition ease-in duration-300 bg-gray-800  hover:text-red-500 shadow hover:shadow-md text-gray-500 rounded-full w-8 h-8 text-center p-1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                        viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
+                                                    </svg>
+                                                </button>
+                                            </form>
+                                            @endif
+
                                         </div>
                                         <img
                                             src="{{$post->image}}"
