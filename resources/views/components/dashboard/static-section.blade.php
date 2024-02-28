2f9e37c435d06a9ce3e5c9ca2bd8c45d1d57be16
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-28 21:18:16 +0100

    Update static-section.blade.php

--- resources/views/components/dashboard/static-section.blade.php
+++ resources/views/components/dashboard/static-section.blade.php
@@ -101,47 +101,113 @@
             </div>
         </div>
         {{-- end mini cards --}}
-
-        <!-- cards row 2 -->
-        <div class="flex flex-wrap mt-6 -mx-3">
-            <div class="w-full px-3 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
-                <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
-                    <div class="flex-auto p-4">
-                        <div class="flex flex-wrap -mx-3">
-                            <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
-                                <div class="flex flex-col h-full">
-                                    <p class="pt-2 mb-1 font-semibold">Built by developers</p>
-                                    <h5 class="font-bold">Soft UI Dashboard</h5>
-                                    <p class="mb-12">From colors, cards, typography to complex elements, you will find the full documentation.</p>
-                                    <a class="mt-auto mb-0 font-semibold leading-normal text-sm group text-slate-500" href="javascript:;">
-                                        Read More
-                                        <i class="fas fa-arrow-right ease-bounce text-sm group-hover:translate-x-1.25 ml-1 leading-normal transition-all duration-200"></i>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="max-w-full px-3 mt-12 ml-auto text-center lg:mt-0 lg:w-5/12 lg:flex-none">
-                                <div class="h-full bg-gradient-to-tl from-purple-700 to-pink-500 rounded-xl">
-                                    <img src="../assets/img/shapes/waves-white.svg" class="absolute top-0 hidden w-1/2 h-full lg:block" alt="waves" />
-                                    <div class="relative flex items-center justify-center h-full">
-                                        <img class="relative z-20 w-full pt-6" src="../assets/img/illustrations/rocket-white.png" alt="rocket" />
-                                    </div>
-                                </div>
+        <div class="flex flex-wrap mt-10 lg:flex-row justify-between">
+            <div class="flex flex-wrap -mx-3">
+                <div class="flex-none w-full max-w-full px-3" style="width: 605px;">
+                    <div
+                        class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                        <div class="flex-auto px-0 pt-0 pb-2">
+                            <div class="p-0 overflow-x-auto">
+                                <table
+                                    class="items-center justify-center w-full mb-0 align-top border-collapse dark:border-white/40 text-slate-500">
+                                    <thead class="align-bottom">
+                                    <tr>
+                                        <th class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            Link id
+                                        </th>
+                                        <th class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            Link name
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="border-t">
+                                    @foreach($data['links'] as $link)
+                                        <tr>
+                                            <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                                <div class="flex px-2">
+                                                    <div class="my-auto">
+                                                        <h6 class="mb-0 text-sm leading-normal dark:text-white">{{$link->id}}</h6>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                                <p class="mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60">
+                                                    {{$link->name}}</p>
+                                            </td>
+                                            <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                                {{-- <form action="/deleteLink/{{$link->id}}" method="post">
+                                                    @csrf
+                                                    <button
+                                                        class="bg-transparent cursor-pointer flex flex-row font-medium text-red-500 px-2 py-1 mt-2 rounded-md transition duration-150"
+                                                        type="submit">
+                                                        <svg class="w-5 h-5 mx-1" fill="none" stroke="currentColor"
+                                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                                                        </svg>
+                                                        Delete
+                                                    </button>
+                                                </form> --}}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </table>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="w-full max-w-full px-3 lg:w-5/12 lg:flex-none">
-                <div class="border-black/12.5 shadow-soft-xl relative flex h-full min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border p-4">
-                    <div class="relative h-full overflow-hidden bg-cover rounded-xl" style="background-image: url('../assets/img/ivancik.jpg')">
-                        <span class="absolute top-0 left-0 w-full h-full bg-center bg-cover bg-gradient-to-tl from-gray-900 to-slate-800 opacity-80"></span>
-                        <div class="relative z-10 flex flex-col flex-auto h-full p-4">
-                            <h5 class="pt-2 mb-6 font-bold text-white">Work with the rockets</h5>
-                            <p class="text-white">Wealth creation is an evolutionarily recent positive-sum game. It is all about who take the opportunity first.</p>
-                            <a class="mt-auto mb-0 font-semibold leading-normal text-white group text-sm" href="javascript:;">
-                                Read More
-                                <i class="fas fa-arrow-right ease-bounce text-sm group-hover:translate-x-1.25 ml-1 leading-normal transition-all duration-200"></i>
-                            </a>
+            <div class="flex flex-wrap -mx-3">
+                <div class="flex-none w-full max-w-full px-3" style="width: 605px;">
+                    <div
+                        class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                        <div class="flex-auto px-0 pt-0 pb-2">
+                            <div class="p-0 overflow-x-auto">
+                                <table
+                                    class="items-center justify-center w-full mb-0 align-top border-collapse dark:border-white/40 text-slate-500">
+                                    <thead class="align-bottom">
+                                    <tr>
+                                        <th class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            Link id
+                                        </th>
+                                        <th class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                            Link name
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="border-t">
+                                    @foreach($data['categories'] as $category)
+                                        <tr>
+                                            <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                                <div class="flex px-2">
+                                                    <div class="my-auto">
+                                                        <h6 class="mb-0 text-sm leading-normal dark:text-white">{{$category->id}}</h6>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                                <p class="mb-0 text-sm font-semibold leading-normal dark:text-white dark:opacity-60">
+                                                    {{$category->name}}</p>
+                                            </td>
+                                            <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                                {{-- <form action="/deleteLink/{{$link->id}}" method="post">
+                                                    @csrf
+                                                    <button
+                                                        class="bg-transparent cursor-pointer flex flex-row font-medium text-red-500 px-2 py-1 mt-2 rounded-md transition duration-150"
+                                                        type="submit">
+                                                        <svg class="w-5 h-5 mx-1" fill="none" stroke="currentColor"
+                                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                                                        </svg>
+                                                        Delete
+                                                    </button>
+                                                </form> --}}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
