930fa98f46320d55b512e97875741f933af3d7d2
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-28 22:35:58 +0100

    Update favorisController.php

--- app/Http/Controllers/favorisController.php
+++ app/Http/Controllers/favorisController.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Favoris;
+use App\Models\Categories;
 use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
+
 
 
 class favorisController extends Controller
@@ -52,5 +55,30 @@ class favorisController extends Controller
                 ->delete();
 
         return redirect()->back();
+    }
+
+    public function showFavorites()
+    {
+        $categories = Cache::remember('categories', 60, function () {
+            return Categories::all();
+        });    
+    
+        $postsByCategory = [];
+    
+        foreach ($categories as $category) {
+            $posts = Cache::remember('posts_' . $category->id, 60, function () use ($category) {
+                return Post::where('category_id', $category->id)->limit(6)->get();
+            });
+    
+            $postsByCategory[$category->name] = $posts;
+        }
+    
+        $user = Auth::user();
+        $favoris = Favoris::where('user_id', $user->id)->pluck('post_id');
+    
+        // Maintenant, récupérez les détails complets des postes aimés
+        $favoritePosts = Post::whereIn('id', $favoris)->get();
+    
+        return view('News.favorites', compact('postsByCategory', 'categories', 'favoritePosts'));
     }
 }
