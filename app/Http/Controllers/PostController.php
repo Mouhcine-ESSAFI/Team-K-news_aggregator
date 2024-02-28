5e20aadc4774df0aee0bdbbe606dd2f8926e8598
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-28 12:31:40 +0100

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,13 @@ use App\Models\Categories;
 use App\Models\Post;
 use App\Models\Preference;
 use App\Models\SourceRss;
+use App\Models\Favoris;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use SebastianBergmann\CodeCoverage\Report\Xml\Source;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Auth;
 
 
 class PostController extends Controller
@@ -20,23 +22,29 @@ class PostController extends Controller
      * this function retrieves categories and posts from cache if they are not in the cache or the cache is expired it retrieves them from the database and caches them
      * 
      */
-    public function showPosts(){
-    $categories = Cache::remember('categories', 60, function () {
-        return Categories::all();
-    });
-
-    $postsByCategory = [];
-
-    foreach ($categories as $category) {
-        $posts = Cache::remember('posts_' . $category->id, 60, function () use ($category) {
-            return Post::where('category_id', $category->id)->limit(6)->get();
+    public function showPosts()
+    {
+        $categories = Cache::remember('categories', 60, function () {
+            return Categories::all();
         });
-
-        $postsByCategory[$category->name] = $posts;
+    
+        $postsByCategory = [];
+    
+        foreach ($categories as $category) {
+            $posts = Cache::remember('posts_' . $category->id, 60, function () use ($category) {
+                return Post::where('category_id', $category->id)->limit(6)->get();
+            });
+    
+            $postsByCategory[$category->name] = $posts;
+        }
+    
+        $user = Auth::user();
+    
+        $favoris = Favoris::where('user_id', $user->id)->pluck('post_id');
+    
+        return view('News.collectionPage', compact('postsByCategory', 'categories', 'favoris'));
     }
-
-    return view('News.collectionPage', compact('postsByCategory', 'categories'));
-}
+    
 
     public function insertPost(Request $r)
     {
